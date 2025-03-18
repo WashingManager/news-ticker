e5d26910411ea03d9d4bb93c7fd1b9545a9854c1
청소부장 <washingmanager2@gmail.com>
2025-03-18 14:01:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-window.SHEET_ID='1XnyaKPBsRDc82-13qsmhJ12t65x9xAMqbzOi19l2QvQ'; window.API_KEY='AIzaSyDngj65pyQb1CmsMQaEooc11osDSZdpl7Q';
+window.SHEET_ID = "{{ secrets.SHEET_ID }}";
+window.API_KEY = "{{ secrets.API_KEY }}";
